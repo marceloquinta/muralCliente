180079f73a12fbbf8e85f1ab46aa4272e8918a5b
erickriger <erickriger@gmail.com>
2014-12-09 02:34:52 -0200

    Exclusão de um import esquecido

--- CentralUFG/app/src/main/java/muralufg/fabrica/inf/ufg/br/centralufg/classificados/fragments/ClassificadosFragment.java
+++ CentralUFG/app/src/main/java/muralufg/fabrica/inf/ufg/br/centralufg/classificados/fragments/ClassificadosFragment.java
@@ -61,7 +61,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.ExpandableListView;
 
 import com.android.volley.Response;
